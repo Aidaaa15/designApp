1272a88ba22fa10edb8875f995d02339874901ae
MasThor <slinxray15@gmail.com>
2022-08-08 13:38:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/designapp/cobaFragment.kt
@@ -0,0 +1,19 @@
+package com.example.designapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class cobaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coba, container, false)
+    }
+
+}
